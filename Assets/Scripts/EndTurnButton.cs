dd76340886dd62c34b9fa3b7e78d9081d1a31336
Ernest Surys <ernest.surys@gmail.com>
2019-06-16 10:38:17 +0200

    Init

---
+++ Assets/Scripts/EndTurnButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Arena.UI
+{
+    // For Each Player
+    [RequireComponent(typeof(Button))]
+    public class EndTurnButton : MonoBehaviour
+    {
+        [SerializeField]
+        private Button _button;
+        
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+        }
+
+        private void OnClick()
+        {
+            
+        }
+    }
+}
+
+namespace Arena
+{
+    // Synced Between players
+    public static class Game
+    {
+        public static Player[] PresentPlayers;
+
+        public static Player ActiveTurn = Player.None;
+        
+        public enum Player
+        {
+            None,
+            Red,
+            Green,
+            Yellow,
+            Blue
+        }
+    }
+}
