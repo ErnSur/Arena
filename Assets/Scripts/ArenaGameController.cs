dfb43a6e5021405590a53077a96d88e1ac365581
Ernest Surys <ernest.surys@gmail.com>
2019-06-16 13:42:55 +0200

    Room Joining logic

--- Assets/Scripts/ArenaGameController.cs
+++ Assets/Scripts/ArenaGameController.cs
@@ -17,27 +17,57 @@ namespace Arena
         private void Awake()
         {
             Instance = this;
-            PhotonNetwork.ConnectUsingSettings();
-            PhotonNetwork.JoinOrCreateRoom("DemoRoom", new RoomOptions() { MaxPlayers = 4 },null);
         }
 
         public override void OnConnected()
         {
-            PhotonNetwork.Instantiate(_playerPrefab.name, new Vector3(0f,0f,0f), Quaternion.identity);
+            Debug.Log($"OnConnected");
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log($"OnConnectedToMaster");
+            PhotonNetwork.JoinRandomRoom();
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log($"Joined to Room");
+            PhotonNetwork.Instantiate(_playerPrefab.name, new Vector3(0f, 0f, 0f), Quaternion.identity);
+            base.OnJoinedRoom();
+        }
+        
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+            // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+            PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 4});
         }
 
         public void SpawnPlayer()
         {
+            if (PhotonNetwork.IsConnected)
+            {
+                Debug.Log($"PLayer Is Connected already");
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+                // #Critical, we must first and foremost connect to Photon Online Server.
+                PhotonNetwork.GameVersion = "version1";
+                PhotonNetwork.ConnectUsingSettings();
+            }
         }
     }
-    
+
     // Synced Between players
     public static class Game
     {
         public static Player[] PresentPlayers;
 
         public static Player ActiveTurn = Player.None;
-        
+
         public enum Player
         {
             None,
